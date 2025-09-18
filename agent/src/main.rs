5388726f8f56e825937efcb8a633f85803349ff4
Séafra Forder <seafraf@gmail.com>
2025-09-18 22:25:17 +0200

    added claims support

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -1,7 +1,8 @@
 use clap::Parser;
 use coral_rs::agent::Agent;
 use coral_rs::agent_loop::AgentLoop;
-use coral_rs::api::generated::types::McpToolName;
+use coral_rs::api::generated::types::{AgentClaimAmount, McpToolName};
+use coral_rs::claim_manager::ClaimManager;
 use coral_rs::completion_evaluated_prompt::CompletionEvaluatedPrompt;
 use coral_rs::mcp_server::McpConnectionBuilder;
 use coral_rs::repeating_prompt_stream::repeating_prompt_stream;
@@ -72,8 +73,13 @@ async fn main() {
         preamble = preamble.string(prompt_suffix);
     }
 
+    let claim_manager = ClaimManager::new()
+        .mil_input_token_cost(AgentClaimAmount::Usd(1.250))
+        .mil_output_token_cost(AgentClaimAmount::Usd(10.000));
+
     let mut agent = Agent::new(completion_agent)
         .preamble(preamble)
+        .claim_manager(claim_manager)
         .mcp_server(coral)
         .mcp_server(context7);
 
